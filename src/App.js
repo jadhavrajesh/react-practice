8f160892bbcfbdaeac251804095e37bd90e3410c
Rajesh Jadhav <Rajesh.Jadhav@viacomcontractor.com>
2019-11-14 11:00:43 +0530

    Added context example, completed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { MyContext } from './concepts/context/ex1';
 
-class AppProvider extends Component {
-  render() {
-    return <MyContext.Provider value='this is my first context example'>
-      AppProvider      
-    </MyContext.Provider>;
-  }
-}
-
 function A() {
   return (
-    <div style={{ "border": "1px solid red"}}>
-      <p>A</p>
+    <div>      
       <MyContext.Consumer>
         {(context) => {
           console.log('context:', context);
-          return 'Consumer:'+context;
+          return 'In Consumer, The context value is : '+context;
         }}
       </MyContext.Consumer>                  
     </div>
   );
 }
 
-function B() {
-  return (
-    <div style={{"border": "1px solid green"}}>
-      <p>B</p>  
-      <AppProvider/>     
-      <A />
-    </div>
-  );
-}
-
-// function C() {
-//   return (
-//     <div style={{"border": "1px solid blue"}}>
-//       <p>C</p>      
-//       <A />           
-//     </div>
-//   );
-// }
-
 function App() {
   return (
     <div className="App"> 
-      <B/>
+      <MyContext.Provider value='this is my first context example'>     
+        <A />
+      </MyContext.Provider>
     </div>
   );
 }
--- src/concepts/context/ex1/context.js
+++ src/concepts/context/ex1/context.js
@@ -1,6 +1,4 @@
 import React from 'react';
 
-export const MyContext = React.createContext(
-    // 'default value'
-    // optional default value
-);
+export const MyContext = React.createContext('default value');
+// optional default value
